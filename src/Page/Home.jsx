880329424105023549e920ae151bc206bf41df23
kryptify <xinzhe1219@outlook.com>
2022-10-15 11:05:46 +0900

    Update

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -3,32 +3,32 @@ import { useEffect } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 
 export default function Home() {
-  // useEffect(() => {
-  //   useNavigate();
-  // }, [useNavigate]);
+  useEffect(() => {
+    useNavigate();
+  }, [useNavigate]);
   const navigate = useNavigate();
   const mintButton = () => {
     return (
-      <a href="/Mint">
-        <button
-          // onClick={() => navigate("/Mint")}
-          className="my-10 rounded bg-gray-500 py-2 px-4 text-3xl font-bold text-white hover:bg-gray-900"
-        >
-          Mint Quadrata Passport
-        </button>
-      </a>
+      // <a href="/Mint">
+      <button
+        onClick={() => navigate("/Mint")}
+        className="my-10 rounded bg-gray-500 py-2 px-4 text-3xl font-bold text-white hover:bg-gray-900"
+      >
+        Mint Quadrata Passport
+      </button>
+      // </a>
     );
   };
   const faucetButton = () => {
     return (
-      <a href="/Faucet">
-        <button
-          onClick={() => navigate("/Faucet")}
-          className="my-10 rounded bg-gray-500 py-2 px-4 text-3xl font-bold text-white hover:bg-gray-900"
-        >
-          Faucet Demo
-        </button>
-      </a>
+      // <a href="/Faucet">
+      <button
+        onClick={() => navigate("/Faucet")}
+        className="my-10 rounded bg-gray-500 py-2 px-4 text-3xl font-bold text-white hover:bg-gray-900"
+      >
+        Faucet Demo
+      </button>
+      // </a>
     );
   };
 
